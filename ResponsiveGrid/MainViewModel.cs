4d935fd652d95b7555b6db8b680d283cd6b9c021
Tareq Ateik <tariq_atiek@live.com>
2016-08-17 15:27:29 +0400

    Add project files.

---
+++ ResponsiveGrid/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiveGrid
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<string> items = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Items
+        {
+            get
+            {
+                return items;
+            }
+
+            set
+            {
+                if (value != items)
+                {
+                    value = items;
+                    if (PropertyChanged != null)
+                        PropertyChanged(this, new PropertyChangedEventArgs("Items"));
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            for (var i = 0; i < 10; i++)
+                Items.Add("http://lorempixel.com/150/150/sports/" + i);
+            for (var i = 0; i < 10; i++)
+                Items.Add("http://lorempixel.com/150/150/city/" + i);
+        }
+    }
+}
